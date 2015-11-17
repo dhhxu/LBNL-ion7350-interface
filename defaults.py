5d99255ba76e0878b06a7c1bf74b8fc0a96f51a4
Daniel Xu <dhhxu88@gmail.com>
2015-11-16 18:32:54 -0800

    Add default path for credential file

--- defaults.py
+++ defaults.py
@@ -6,5 +6,12 @@ import os
 
 cwd = os.getcwd()
 
+# Directory to store downloaded meter reading data.
 DOWNLOADS = os.path.join(cwd, 'downloaded_data')
+
+# Directory to store generated json files for future push to BuildingOS.
 JSON_DIR = os.path.join(cwd, 'json_dir')
+
+# Read only file containing database login credentials
+CREDS = os.path.join(cwd, '.ioninfo')
+
