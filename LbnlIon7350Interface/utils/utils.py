24b015fe024056899659a8e1a8f101568e46efaa
Daniel Xu <dhhxu88@gmail.com>
2015-11-16 18:33:22 -0800

    Move path checking to utils file

--- LbnlIon7350Interface/utils/utils.py
+++ LbnlIon7350Interface/utils/utils.py
@@ -2,10 +2,31 @@
 Collection of utility methods.
 """
 
+import os
+
 def error(msg):
     """
     Print msg with [ERROR] appended.
     """
 
     print('[ERROR] %s' % (msg))
+
+
+def exists_dir(path):
+    """
+    Returns true if path exists.
+
+    Params:
+        path string
+    """
+    return os.path.isdir(path)
+
+def exists_file(path):
+    """
+    Returns true if file at path path exists.
+
+    Params:
+        path string
+    """
+    return os.path.isfile(path)
 
