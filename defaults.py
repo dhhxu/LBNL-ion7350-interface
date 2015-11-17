f5ab7d93a60956d26fd26b6969f50241b0a5866e
Daniel Xu <dhhxu88@gmail.com>
2015-11-17 15:15:50 -0800

    Add meter file path

--- defaults.py
+++ defaults.py
@@ -6,6 +6,8 @@ import os
 
 cwd = os.getcwd()
 
+PROJECT = 'LbnlIon7350Interface'
+
 # Directory to store downloaded meter reading data.
 DOWNLOADS = os.path.join(cwd, 'downloaded_data')
 
@@ -13,5 +15,8 @@ DOWNLOADS = os.path.join(cwd, 'downloaded_data')
 JSON_DIR = os.path.join(cwd, 'json_dir')
 
 # Read only file containing database login credentials
-CREDS = os.path.join(cwd, '.ioninfo')
+CREDS = os.path.join(cwd, PROJECT, '.ioninfo')
+
+# File containing ION 7350 meter information
+METER_FILE = os.path.join(cwd, PROJECT, 'ion7350meters.csv')
 
