a5e59fd17b3888c4d3681239c2e4d2fd1afb8469
Daniel Xu <dhhxu88@gmail.com>
2015-12-10 17:20:17 -0800

    Fix typo

--- interface.py
+++ interface.py
@@ -2,7 +2,7 @@
 
 """
 This is the updater script for the ION 7350 interface to BuildingOS. Every X hours, where
-X is some number between 2 and 12 (larger intervals are not good), downloads reading data
+X is some number between 1 and 12 (larger intervals are not good), downloads reading data
 from the past X hours for each meter listed in an input csv file, packages it into a JSON
 object as per Lucid's Connector API, and pushes that object to BuildingOS.
 """
