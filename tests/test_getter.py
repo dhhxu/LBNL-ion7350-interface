98a27976746fa9e39950ca0295b61a806bcc6d37
Daniel Xu <dhhxu88@gmail.com>
2016-01-04 15:13:05 -0800

    Add more tests for getter and utils

--- tests/test_getter.py
+++ tests/test_getter.py
@@ -1,20 +1,111 @@
+"""
+Unit tests for getter.py
+"""
+
+import pytest
 from LbnlIon7350Interface.LbnlIon7350Interface import getter
 
 def test_is_valid_interval():
-    assert getter.is_valid_interval(5)
-    assert not getter.is_valid_interval(0)
-    assert not getter.is_valid_interval(1)
-    assert not getter.is_valid_interval(13)
-
+    """
+    Check that is_valid_interval works correctly for legal interval values
+    """
+    assert getter.is_valid_interval(1)
     assert getter.is_valid_interval(2)
     assert getter.is_valid_interval(12)
 
+def test_invalid_interval():
+    """
+    Check that is_valid_interval works correctly for illegal interval values
+    """
+    assert getter.is_valid_interval(5)
+    assert not getter.is_valid_interval(0)
+    assert not getter.is_valid_interval(13)
+
 def test_get_date():
-    valid = getter.get_date('2012-01-01 23:59:59')
+    """
+    Check that get_date works correctly for valid datetime.
+    """
+    valid = getter.get_date('2012-01-01T23:59:59')
     assert valid
 
+def test_get_date_bad():
+    """
+    Check that get_date raises error for only date input.
+    """
     invalid = getter.get_date('2012-01-01')
     assert not invalid
 
-    invalid2 = getter.get_date('2012-13-01 23:59:59')
+def test_get_date_bad2():
+    """
+    Check that get_date raises error for datetime missing 'T' between the
+    date and time.
+    """
+    invalid2 = getter.get_date('2012-01-01 23:59:59')
     assert not invalid2
+
+def test_get_date_bad3():
+    """
+    Check that get_date raises error for datetime containing erroneous hour.
+    """
+    invalid3 = getter.get_date('2012-01-01T24:59:59')
+    assert not invalid3
+
+def test_run_batch_bad_date():
+    """
+    Check that run_batch raises error for an invalid start date argument
+    """
+    date = '2015-20-01T00:00:00'
+    root = '~/'
+    end = '2015-01-02T00:00:00'
+    with pytest.raises(ValueError) as bad_date:
+        getter.run_batch(root, date, end)
+
+    assert bad_date.value.message == 'Invalid/missing dates'
+
+def test_run_batch_bad_date2():
+    """
+    Check that run_batch raises error for an invalid end date argument
+    """
+    date = '2015-20-01T00:00:00'
+    root = '/'
+    start = '2015-01-01T00:00:00'
+    with pytest.raises(ValueError) as bad_date:
+        getter.run_batch(root, start, date)
+
+    assert bad_date.value.message == 'Invalid/missing dates'
+
+def test_run_batch_bad_date_order():
+    """
+    Check that run_batch raises error when end date comes before start date
+    """
+    root = '/'
+    start = '2015-01-01T00:00:00'
+    end = '2015-01-02T00:00:00'
+    with pytest.raises(ValueError) as bad_order:
+        getter.run_batch(root, end, start)
+
+    assert bad_order.value.message == 'Start date must come before end date'
+
+def test_run_batch_bad_root():
+    """
+    Check that run_batch raises error for non-existent root path argument
+    """
+    root = 'blahasdfjadksx'
+    start = '2015-01-01T00:00:00'
+    end = '2015-01-02T00:00:00'
+    with pytest.raises(ValueError) as bad_root:
+        getter.run_batch(root, start, end)
+
+    assert bad_root.value.message == 'Root directory not found'
+
+def test_run_update_bad_interval():
+    """
+    Check that run_update raises error for illegal interval argument
+    """
+    root = '~/'
+    interval = -1
+    with pytest.raises(ValueError) as bad_interval:
+        getter.run_update(root, interval)
+
+    assert bad_interval.value.message == 'Invalid interval'
+
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,20 +1,55 @@
-import LbnlIon7350Interface.LbnlIon7350Interface.utils.utils as utils
+"""
+Unit tests for utils.py
+"""
 
 from os import remove
 
-def test_exists_dir():
+import LbnlIon7350Interface.LbnlIon7350Interface.utils.utils as utils
+
+def test_not_exists_dir():
+    """
+    Check that exists_dir returns False for nonexistent path
+    """
     nonexistent = '~/blahblah/'
     assert not utils.exists_dir(nonexistent)
 
-    existent = '/home/daniel/Downloads/'
+def test_exists_dir():
+    """
+    Check that exists_dir returns True for valid path
+    """
+    existent = '/'
     assert utils.exists_dir(existent)
 
-def test_exists_file():
+def test_not_exists_file():
+    """
+    Check that exists_file returns False for nonexistent file
+    """
     nonexistent = '~/blabjfajdfd'
     assert not utils.exists_file(nonexistent)
+
+def test_exists_file():
+    """
+    Check that exists_file returns True for a temp file.
+    The temp file is deleted after this test finishes.
+    """
     existent = 'dummy.txt'
     with open(existent, 'wb') as f:
         f.write('hello world!')
         assert utils.exists_file(existent)
         assert not utils.exists_dir(existent)
-    remove(existent)
+        remove(existent)
+
+def test_get_cnxn_str():
+    """
+    Check that get_cnxn_str properly reads from a temporary credentials file
+    and returns a correct login string.
+    """
+    tmp = 'creds.txt'
+    with open(tmp, 'wb') as cf:
+        cf.write('user\n')
+        cf.write('password')
+    cnxn_str = utils.get_cnxn_str(tmp)
+    assert cnxn_str == 'DSN=ION;UID=user;PWD=password'
+
+    remove(tmp)
+
