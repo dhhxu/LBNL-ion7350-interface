75969c750824088f1ae414953dd84528bb1dea16
Daniel Xu <dhhxu88@gmail.com>
2016-02-11 11:22:17 -0800

    Remove trailing whitespace

--- tests/test_getter.py
+++ tests/test_getter.py
@@ -21,14 +21,14 @@ def test_invalid_interval():
     assert not getter.is_valid_interval(13)
     assert not getter.is_valid_interval(5.0)
     assert not getter.is_valid_interval(-4)
-    
+
 def test_valid_index():
     """
     Check that is_valid_index works correctly for non-negative integers
     """
     assert getter.is_valid_index(1)
     assert getter.is_valid_index(100)
-    
+
 def test_invalid_index():
     """
     Check that is_valid_index works correctly for negative integers and
